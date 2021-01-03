97a7d1dc76fd4fae7ad98f0a08fabd255f90f2ae
Bce <barkancanerdogdu@gmail.com>
2021-01-03 18:51:48 +0300

    properties and some page modifications. also data transferobject has been created for contacts and api endpoint has been written

--- src/main/java/com/website/bce/controller/ApplicationController.java
+++ src/main/java/com/website/bce/controller/ApplicationController.java
@@ -1,18 +1,20 @@
 package com.website.bce.controller;
 
+import com.website.bce.dtos.Contact;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ApplicationController {
 
     @GetMapping("/")
-    public String greetings() {
-        return "bce";
-    }
-
-    @GetMapping("/index")
     public String index() {
         return "index";
+    }
+
+    @PostMapping(path = "/email/me", consumes = "application/x-www-form-urlencoded")
+    public String email(Contact contact) {
+        return "thanks";
     }
 }
---
+++ src/main/java/com/website/bce/rest/RestAPI.java
@@ -0,0 +1,8 @@
+package com.website.bce.rest;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RestAPI {
+
+}
