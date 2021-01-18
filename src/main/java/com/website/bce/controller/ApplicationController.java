2b87bec717d5e64f3365f13e1399a3c2be508ab1
bce <barkancanerdogdu@gmail.com>
2021-01-18 22:32:59 +0300

    indentation

--- src/main/java/com/website/bce/controller/ApplicationController.java
+++ src/main/java/com/website/bce/controller/ApplicationController.java
@@ -1,16 +1,13 @@
 package com.website.bce.controller;
 
 import com.website.bce.dto.PrincipalDto;
-
 import com.website.bce.service.PrincipalService;
 import com.website.bce.service.VisitorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
 import javax.servlet.http.HttpServletRequest;
-import java.util.Date;
 
 @Controller
 public class ApplicationController {
