e366e40daaa0666a427b1738b0c088658c63344c
bce <barkancanerdogdu@gmail.com>
2021-01-23 15:14:07 +0300

    another property file has been added for prod environment, index.html context has been changed, VisitorService class exception handling has been fixed

--- src/main/java/com/website/bce/service/VisitorService.java
+++ src/main/java/com/website/bce/service/VisitorService.java
@@ -9,6 +9,7 @@ import com.website.bce.repository.VisitorDao;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,6 +17,7 @@ import java.io.File;
 import java.io.IOException;
 import java.net.InetAddress;
 import java.util.Date;
+import java.util.Locale;
 import java.util.TimeZone;
 
 @Service
@@ -26,8 +28,11 @@ public class VisitorService {
     @Autowired
     VisitorDao visitorDao;
 
+    @Value("${geolib.path}")
+    private String geolib;
+
     public void visitorClientInfo(HttpServletRequest request) {
-        File database = new File("/root/GeoLite2-City.mmdb");
+        File database = new File(geolib);
         String visitorIp = request.getRemoteAddr();
 
         CityResponse response;
@@ -42,20 +47,22 @@ public class VisitorService {
             DatabaseReader dbReader = new DatabaseReader.Builder(database).build();
             InetAddress inetAddress = InetAddress.getByName(visitorIp);
             response = dbReader.city(inetAddress);
+
             country = response.getCountry().getName();
             city = response.getCity().getName();
             postal = response.getPostal().getCode();
             state = response.getLeastSpecificSubdivision().getName();
             os = request.getHeader(HEADER_USERAGENT);
             ip = request.getRemoteAddr();
+
+            TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
+            Date date = new Date();
+            Visitor visitor = new Visitor(country, city, postal, state, date.toString(), os, ip);
+            visitorDao.save(visitor);
+            LOGGER.info("[Visitor] - IP: " + ip + ", Country: " + country + ", City: " + city + ", Postal: " + postal + ", OS: ", os);
+
         } catch (IOException | GeoIp2Exception e) {
             e.printStackTrace();
         }
-
-        TimeZone.setDefault(TimeZone.getTimeZone("GMT"));
-        Date date = new Date();
-        Visitor visitor = new Visitor(country, city, postal, state, date.toString(), os, ip);
-        visitorDao.save(visitor);
-        LOGGER.info("[Visitor] - IP: " + ip + ", Country: " + country + ", City: " + city + ", Postal: " + postal + ", OS: ", os);
     }
 }
